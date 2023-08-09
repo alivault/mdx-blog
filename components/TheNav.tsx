3187768554c3470c45ccabea131b9bc2eef95471
Ali Abbas <48610822+alivault@users.noreply.github.com>
2023-08-09 02:13:16 -0700

    fix: nav margin

--- components/TheNav.tsx
+++ components/TheNav.tsx
@@ -5,7 +5,7 @@ import NavLink from './NavLink';
 import React from 'react';
 export default function TheNav({}) {
   return (
-    <nav className='mx-auto flex w-full max-w-[640px] items-start lg:mb-6'>
+    <nav className='mx-auto flex w-full max-w-[640px] items-start'>
       <ul className='flex flex-row gap-4'>
         <li className='flex items-center gap-2'>
           <Link href='/'>
