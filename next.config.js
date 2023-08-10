82934f226ccc5bb5db115a35b2366fd7f885c525
Ali Abbas <48610822+alivault@users.noreply.github.com>
2023-08-09 17:33:22 -0700

    fix shiki

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
   },
   experimental: {
     outputFileTracingIncludes: {
+      '/': ['node_modules/shiki/**/*'],
       '/articles/[articleId]': ['node_modules/shiki/**/*'],
     },
   },
