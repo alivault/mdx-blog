7b13db19a04c3a4ef639402cca2ec89486c8113c
Ali Abbas <48610822+alivault@users.noreply.github.com>
2023-08-11 17:30:21 -0700

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Ali Abbas',
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className='mx-auto max-w-3xl bg-background p-4 text-foreground lg:p-6'>
         {children}
+        <Analytics />
       </body>
     </html>
   );
